f9369d595498728ad87f675857ffc02df30217ee
LenaNeugebauer <lena.neugebauer102@gmail.com>
2022-06-19 22:01:26 +0200

    Add HomePageTest

--- src/test/java/com/telran/oscar/pages/PageBase.java
+++ src/test/java/com/telran/oscar/pages/PageBase.java
@@ -1,12 +1,10 @@
 package com.telran.oscar.pages;
 
 import com.google.common.io.Files;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
@@ -22,7 +20,6 @@ public class PageBase {
     }
 
     public void click(WebElement element) {
-
         element.click();
     }
 
@@ -74,5 +71,28 @@ public class PageBase {
         new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOf(element));
     }
 
-    
+    public void scrollUpOrDown(int x, int y){
+        JavascriptExecutor js =(JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy("+ x + ","+ y + ")");
+    }
+
+    public void selectDropDownByValue(WebElement element, String value){
+        Select select = new Select(element);
+        select.selectByValue(value);
+    }
+
+    public boolean isElementClickable(WebElement element, int timeout){
+
+        WebElement elementTemp = null;
+        try{
+            new WebDriverWait(driver,timeout).until(ExpectedConditions.elementToBeClickable(element));
+            return true;
+
+        }catch (WebDriverException wde){
+            System.out.println(wde);
+        }
+
+        return false;
+    }
+
 }
--- src/test/java/com/telran/oscar/tests/HomePageTests.java
+++ src/test/java/com/telran/oscar/tests/HomePageTests.java
@@ -1,26 +1,78 @@
 package com.telran.oscar.tests;
 
-import com.telran.oscar.pages.HomePage;
-import com.telran.oscar.pages.ProductPage;
+import com.telran.oscar.pages.basket.BasketPage;
+import com.telran.oscar.pages.home.HomePage;
+import com.telran.oscar.pages.home.SidePanelPage;
+import com.telran.oscar.product.ProductMatchingPage;
+import com.telran.oscar.product.ProductPage;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HomePageTests extends TestBase{
+public class HomePageTests extends TestBase {
 
     HomePage homePage;
     ProductPage productPage;
+    SidePanelPage sidePanelPage;
+    ProductMatchingPage productMatchingPage;
+    BasketPage basketPage;
 
     @BeforeMethod
     public void pageInit() {
         homePage = PageFactory.initElements(driver, HomePage.class);
         productPage = PageFactory.initElements(driver, ProductPage.class);
+        sidePanelPage = PageFactory.initElements(driver, SidePanelPage.class);
+        productMatchingPage = PageFactory.initElements(driver, ProductMatchingPage.class);
+        basketPage = PageFactory.initElements(driver,BasketPage.class);
+        homePage.selectLanguage("en-gb");
     }
 
     @Test
     public void userCanSelectItemFromMenu() {
-        homePage.selectCategory();
+        sidePanelPage.clickOnBooksTab();
         Assert.assertTrue(productPage.isProductPage().contains("Books"));
+    }
+
+    @Test
+    public void allProductsPageTest() {
+        sidePanelPage.clickOnAllProductsTab();
+        Assert.assertTrue(productPage.isProductPageAllProducts().contains("All products"));
+    }
+
+    @Test
+    public void clothingPageTest() {
+        sidePanelPage.clickOnClothingTab();
+        Assert.assertTrue(productPage.isProductPageClothing().contains("Clothing"));
+    }
+
+    @Test
+    public void offersPageTest() {
+        sidePanelPage.clickOnOffersTab();
+        Assert.assertTrue(productPage.isProductPageOffers().contains("Offers"));
+    }
+
+    @Test
+    public void searchPositiveTest() {
+        homePage.typeOnSearchField("Reversing");
+        Assert.assertTrue(productMatchingPage.isProductsMatching().contains("Reversing"));
+    }
+
+    @Test
+    public void linksClickableTest(){
+        Assert.assertTrue(homePage.pageTitleClickable());
+        Assert.assertTrue(homePage.productLinkClickable());
+    }
+
+    @Test
+    public void checkProductPriceInBasket() {
+        homePage.clickOnAddToBasket();
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            int priceAddToBasket = i;
+            int priceBasketTotal = i;
+            if (priceAddToBasket != priceBasketTotal) {
+                System.out.println("Error: The total amount is not equal to the order amount");
+            }
+        }
     }
 }
